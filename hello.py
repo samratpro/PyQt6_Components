4ddf69383b5077f45e41724706a632de52443d31
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-09-07 18:15:24 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,26 @@
+# Functional Way
+from PyQt6.QtWidgets import *
+import sys
 
+app = QApplication(sys.argv)
+window = QMainWindow()
+window.statusBar().showMessage("Welcome to here")
+window.menuBar().addMenu("Open File")
+
+window.setGeometry(0, 0, 700, 300)
+window.show()
+sys.exit(app.exec())
+
+
+# Class Base
+from PyQt6.QtWidgets import *
+import sys
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(0, 0, 700, 400)
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec())
