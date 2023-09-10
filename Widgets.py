3637a3381c37be09de5da923d652b3ebb92ba676
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-09-11 04:15:25 +0600

    Update Widgets.py

--- Widgets.py
+++ Widgets.py
@@ -3,23 +3,40 @@ from PyQt6.QtGui import *
 from PyQt6 import uic
 import sys
 
-# QLabel
-label = QLabel('Hi 1', self)
-# label.setText('Hi')
-# label.setNum(15)
-# label.move(100, 100)
-label.setStyleSheet('color:blue')
-# label.clear()
 
-pixmap = QPixmap('py.png')
-label.setPixmap(pixmap)
+class Window(QWidget):  # QWidget....................
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(220, 220, 700, 400)
 
-# QLineEdit 
-line = QLineEdit(self)  # self or Window
-line.setText('Hi 2')
-line.setPlaceholderText("Hi")
-line.setEnabled(True)
-line.setEchoMode(QLineEdit.EchoMode.Password)
+        # Working with label
+        label = QLabel('Hi 1', self)
+        label.setText('Hi')
+        label.setNum(15)
+        label.move(100, 100)
+        label.setStyleSheet('color:blue')
+        label.clear()
+        pixmap = QPixmap('py.png')
+        label.setPixmap(pixmap)
+        movie = QMovie('py.gif')
+        label.setMovie(movie)
+        movie.start()
+
+
+        # QLineEdit 
+        line = QLineEdit(self)  # self or Window
+        line.setText('Hi 2')
+        line.setPlaceholderText("Hi")
+        line.setEnabled(True)
+        line.setEchoMode(QLineEdit.EchoMode.Password)
+
+        
+
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec())
 
 
 
