d1a9713dcb43fd4282e11affc25332a29dfb7ee0
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-09-11 02:35:15 +0600

    Update Widgets.py

--- Widgets.py
+++ Widgets.py
@@ -1,7 +1,25 @@
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6 import uic
+import sys
+
+# QLabel
+label = QLabel('Hi 1', self)
+# label.setText('Hi')
+# label.setNum(15)
+# label.move(100, 100)
+label.setStyleSheet('color:blue')
+# label.clear()
+
+pixmap = QPixmap('py.png')
+label.setPixmap(pixmap)
+
 # QLineEdit 
 line = QLineEdit(self)  # self or Window
 line.setText('Hi 2')
 line.setPlaceholderText("Hi")
 line.setEnabled(True)
 line.setEchoMode(QLineEdit.EchoMode.Password)
+
+
 
