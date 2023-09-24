89248cecb8aded3898c23a789a80612364cbf61f
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-09-25 02:39:02 +0600

    Update Widgets.py

--- Widgets.py
+++ Widgets.py
@@ -18,32 +18,58 @@ class Window(QWidget):  # QWidget....................
         label.move(100, 100)
         label.setStyleSheet('color:blue')
         label.clear()
-        pixmap = QPixmap('py.png')
+
+        #  Add Picture in Label
+        pixmap = QPixmap('py.png')  
         label.setPixmap(pixmap)
-        movie = QMovie('py.gif')
+
+        # Add gif picture in label
+        movie = QMovie('py.gif')  
         label.setMovie(movie)
         movie.start()
 
 
         # QLineEdit 
         line = QLineEdit(self)  # self or Window
+        self.line.setFixedWidth(300)
         line.setText('Hi 2')
         line.setPlaceholderText("Hi")
         line.setEnabled(True)
         line.setEchoMode(QLineEdit.EchoMode.Password)
+        
 
-
-        # Spin Box
+        # Spin Box and QHBoxLayout
         HBox = QHBoxLayout(self)
         self.label = QLabel('Price : ')
         self.spinbox = QSpinBox(self)
-        self.spinbox.valueChanged.connect(self.price_change)  # When User will scroll up price will change
+        self.spinbox.valueChanged.connect(self.price_change)  # When User will scroll up price will change for price_change function
         self.line = QLineEdit(self)
-        self.line.setFixedWidth(300)
         HBox.addWidget(self.label)
         HBox.addWidget(self.spinbox)
         HBox.addWidget(self.line)
+        VBox.addStretch(5)
         self.setLayout(HBox)
+
+        # VBoxLayout
+        VBox = QVBoxLayout(self)
+        self.label = QLabel('V Box Test')
+        self.line = QLineEdit(self)
+        VBox.addWidget(self.label)
+        VBox.addWidget(self.line)
+        VBox.addStretch(5)
+        self.setLayout(VBox)
+
+
+        # Grid Layout, it like tkinter
+        grid = QGridLayout(self)
+        self.label = QLabel('V Box Test')
+        self.line = QLineEdit(self)
+        self.line.setFixedWidth(300)
+        grid.addWidget(self.label, 0, 0)
+        grid.addWidget(self.line, 0, 1)
+        self.setLayout(grid)
+    
+
 
     def price_change(self):
         spin_data = self.spinbox.value()
