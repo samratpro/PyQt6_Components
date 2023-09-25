2ed08237ad07673eb1ed0fce00bb806cf1b8bbf9
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-09-25 07:24:17 +0600

    Update events.py

--- events.py
+++ events.py
@@ -15,6 +15,7 @@ class Window(QWidget):  # QWidget....................
         self.label = QLabel()
         self.line = QLineEdit(self)
         self.line.textChanged.connect(self.label.setText)
+        self.line.editingFinished.connect(self.on_editing_finished)
       
         # Spin Box and QHBoxLayout
         self.spinbox = QSpinBox(self)
@@ -25,42 +26,140 @@ class Window(QWidget):  # QWidget....................
         self.btn = QPushButton('My Data', sel
         self.btn.clicked.connect(self.create_message)   
 
+        # Radio Button               
+        self.radio_button = QRadioButton('Option 1')
+        self.radio_button.toggled.connect(self.on_radio_toggled)
 
-        def mouseMoveEvent(self, e):
-          if e.button == Qt.MouseButton.RightButton:
-            self.label.setText("mouseMoveEvent  ###############")
 
-        def mousePressEvent(self, e):
-          if e.button() == Qt.MouseButton.LeftButton:
-              self.label.setText("mousePressEvent LEFT ..............")
-  
-          elif e.button() == Qt.MouseButton.MiddleButton:
-              self.label.setText("mousePressEvent MIDDLE .........")
-  
-          elif e.button() == Qt.MouseButton.RightButton:
-              self.label.setText("mousePressEvent RIGHT ...............")
-  
-  
-        def mouseReleaseEvent(self, e):
-          if e.button() == Qt.MouseButton.LeftButton:
-              self.label.setText("mouseReleaseEvent LEFT>>>>>>>>>>>>>>>>>>>")
-  
-          elif e.button() == Qt.MouseButton.MiddleButton:
-              self.label.setText("mouseReleaseEvent MIDDLE>>>>>>>>>>>>>")
-  
-          elif e.button() == Qt.MouseButton.RightButton:
-              self.label.setText("mouseReleaseEvent RIGHT>>>>>>>>>>>>>>")
-  
-  
-        def mouseDoubleClickEvent(self, e):
-            if e.button() == Qt.MouseButton.LeftButton:
-                self.label.setText("mouseDoubleClickEvent LEFT<<<<<<<<<<<<<<<<<<<<<<<<")
+        # *************** QTextEdit ********************
+        layout = QVBoxLayout()
+        self.text_edit = QTextEdit()
+        self.text_edit.textChanged.connect(self.on_text_changed)
+        self.text_edit.cursorPositionChanged.connect(self.on_cursor_position_changed)
+        layout.addWidget(self.text_edit)
+        self.setLayout(layout)
+
+
+        # ******************** QCheckBox ********************
+        layout = QVBoxLayout()
+        self.checkbox = QCheckBox('Check me')
+        self.checkbox.stateChanged.connect(self.on_checkbox_state_changed)
+        layout.addWidget(self.checkbox)
+        self.setLayout(layout)
+
+        # *********** Combobox ******************
+        layout = QVBoxLayout()
+        self.combo_box = QComboBox()
+        self.combo_box.addItem('Option 1')
+        self.combo_box.addItem('Option 2')
+        self.combo_box.activated.connect(self.on_activated)
+        self.combo_box.currentIndexChanged.connect(self.on_current_index_changed)
+        layout.addWidget(self.combo_box)
+        self.setLayout(layout)
+
+        # ***************** Q List *********************
+        layout = QVBoxLayout()
+        self.list_widget = QListWidget()
+        self.list_widget.addItem('Item 1')
+        self.list_widget.addItem('Item 2')
+        self.list_widget.itemClicked.connect(self.on_item_clicked)
+        self.list_widget.itemDoubleClicked.connect(self.on_item_double_clicked)
+        self.list_widget.itemSelectionChanged.connect(self.on_item_selection_changed)
+        layout.addWidget(self.list_widget)
+        self.setLayout(layout)
+
+        # **************** Table ******************
+        layout = QVBoxLayout()
+        self.table_widget = QTableWidget()
+        self.table_widget.setRowCount(1)
+        self.table_widget.setColumnCount(1)
+        self.table_widget.setItem(0, 0, QTableWidgetItem('Row 1, Col 1'))
+        self.table_widget.cellClicked.connect(self.on_cell_clicked)
+        self.table_widget.cellDoubleClicked.connect(self.on_cell_double_clicked)
+        self.table_widget.itemSelectionChanged.connect(self.on_item_selection_changed)
+        layout.addWidget(self.table_widget)
+        self.setLayout(layout)
+
+    # *************** QTextEdit ****************
+    def on_text_changed(self): 
+        print("Text changed:", self.text_edit.toPlainText())
+    def on_cursor_position_changed(self):
+        cursor_position = self.text_edit.textCursor().position()
+        print("Cursor position:", cursor_position)
+
+    # ******************** QCheckBox ********************
+    def on_checkbox_state_changed(self, state):
+        if state == 2:  # Checked state
+            print("Check box checked")
+        else:
+            print("Check box unchecked")
+
+    # *********** Combobox ******************
+    def on_activated(self, index):
+        selected_item = self.combo_box.itemText(index)
+        print(f"Activated: {selected_item}")
+    def on_current_index_changed(self, index):
+        selected_item = self.combo_box.itemText(index)
+        print(f"Current Index Changed: {selected_item}") 
+
+
+   # ***************** Q List *********************
+   def on_item_clicked(self, item):
+        print(f"Item clicked: {item.text()}")
+   def on_item_double_clicked(self, item):
+        print(f"Item double-clicked: {item.text()}")
+   def on_item_selection_changed(self):
+        selected_items = [item.text() for item in self.list_widget.selectedItems()]
+        print(f"Selection changed: {selected_items}")
+
+
+   # **************** Table ******************
+   def on_cell_clicked(self, row, column):
+      item_text = self.table_widget.item(row, column).text()
+      print(f"Cell clicked: {item_text}")
+   def on_cell_double_clicked(self, row, column):
+      item_text = self.table_widget.item(row, column).text()
+      print(f"Cell double-clicked: {item_text}")
+   def on_item_selection_changed(self):
+      selected_items = [item.text() for item in self.table_widget.selectedItems()]
+      print(f"Selection changed: {selected_items}")
+
+     
+    def mouseMoveEvent(self, e):
+      if e.button == Qt.MouseButton.RightButton:
+        self.label.setText("mouseMoveEvent  ###############")
     
-            elif e.button() == Qt.MouseButton.MiddleButton:
-                self.label.setText("mouseDoubleClickEvent MIDDLE<<<<<<<<<<<<<<<<<<<<<<")
+    def mousePressEvent(self, e):
+      if e.button() == Qt.MouseButton.LeftButton:
+          self.label.setText("mousePressEvent LEFT ..............")
     
-            elif e.button() == Qt.MouseButton.RightButton:
-                self.label.setText("mouseDoubleClickEvent RIGHT<<<<<<<<<<<<<<<<<")
+      elif e.button() == Qt.MouseButton.MiddleButton:
+          self.label.setText("mousePressEvent MIDDLE .........")
+    
+      elif e.button() == Qt.MouseButton.RightButton:
+          self.label.setText("mousePressEvent RIGHT ...............")
+    
+    
+    def mouseReleaseEvent(self, e):
+      if e.button() == Qt.MouseButton.LeftButton:
+          self.label.setText("mouseReleaseEvent LEFT>>>>>>>>>>>>>>>>>>>")
+    
+      elif e.button() == Qt.MouseButton.MiddleButton:
+          self.label.setText("mouseReleaseEvent MIDDLE>>>>>>>>>>>>>")
+    
+      elif e.button() == Qt.MouseButton.RightButton:
+          self.label.setText("mouseReleaseEvent RIGHT>>>>>>>>>>>>>>")
+    
+    
+    def mouseDoubleClickEvent(self, e):
+        if e.button() == Qt.MouseButton.LeftButton:
+            self.label.setText("mouseDoubleClickEvent LEFT<<<<<<<<<<<<<<<<<<<<<<<<")
+    
+        elif e.button() == Qt.MouseButton.MiddleButton:
+            self.label.setText("mouseDoubleClickEvent MIDDLE<<<<<<<<<<<<<<<<<<<<<<")
+    
+        elif e.button() == Qt.MouseButton.RightButton:
+            self.label.setText("mouseDoubleClickEvent RIGHT<<<<<<<<<<<<<<<<<")
 
 
 app = QApplication(sys.argv)
