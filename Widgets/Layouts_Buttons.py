d9213d64cf16c75d00d08e70298e14276cc97e24
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2023-10-01 02:42:52 +0600

    Update and rename Layouts.py to Layouts_Buttons.py

---
+++ Widgets/Layouts_Buttons.py
@@ -0,0 +1,68 @@
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6 import uic
+import sys
+from PyQt6.QtCore import Qt
+
+
+class Window(QWidget):  # QWidget....................
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(220, 220, 700, 400)
+        self.setWindowTitle('Hello App')
+        self.setWindowIcon(QIcon("py.png"))
+
+      
+        
+        # **************** Q Push Button with event  ****************                 
+        self.btn = QPushButton('My Data', self)
+        self.btn.clicked.connect(self.create_message)      # *** Event
+                              
+    def create_message(self):                
+        new_label = QLabel('New Label')      
+        self.layout().addWidget(new_label)   
+
+
+         # **************** Tool Button ******************      
+
+        
+
+      
+        # **************** Radio Button ******************             
+        self.radio_button = QRadioButton('Option 1')
+        self.radio_button.toggled.connect(self.on_radio_toggled)   # *** Event
+        
+    def radio_operation(self):
+        if self.radio_button.isChecked() == True:
+            print("Radio Button has been checked")
+          
+
+        # ******************** QCheckBox ********************
+        layout = QVBoxLayout()
+        self.checkbox = QCheckBox('Check me')
+        self.checkbox.stateChanged.connect(self.on_checkbox_state_changed)   # *** Event
+        layout.addWidget(self.checkbox)
+        self.setLayout(layout)
+        
+    def on_checkbox_state_changed(self, state):
+        if state == 2:  # Checked state
+            print("Check box checked")
+        else:
+            print("Check box unchecked")
+    
+    def check_box(self):
+        if self.checkbox.isChecked():
+            print("Checked")
+
+
+        # **************** CommandLinkButton ******************    
+
+
+        # **************** Dialog Button Box ******************    
+      
+       
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec())
